018b2a3a78d8e74faa5c072a8b6d36a29fc20e15
Gini5 <gini5@163.com>
2018-09-20 16:33:14 +0800

    set up initial background and load rabbit

---
+++ Rabbit/Rabbit.py
@@ -0,0 +1,41 @@
+import pygame
+import math
+import random
+
+# initilize the game and set display window
+pygame.init()
+width, height = 640, 480
+screen = pygame.display.set_mode((width,height))
+
+# set keys to record if player press down the key: WASD
+keys = [False, False, False, False]
+# current player position
+playerpos = [100, 100]
+# accuracy variable, record number of shoots and shooted animals
+acc = [0, 0]
+# track arrow
+arrows = []
+# set a timer, after a certain period of time, create a new bad animal
+badtimer = 100
+badtimer1 = 0
+badguys = [[640, 100]]
+healthvalue = 194
+
+# initialize sound
+pygame.mixer.init()
+
+# load pictures
+rabbit_img = pygame.image.load("resources/images/dude.png")
+
+while True:
+    # set background to color gray(RGB)
+    screen.fill((224,226,229))
+    # load rabbit pic
+    screen.blit(rabbit_img, (100, 100))
+    # update screen
+    pygame.display.flip()
+    # check some events, if has QUIT command, then exit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
