7f337d7e44e46ab2fc13c1e88a1ea2e54ddb7be8
Gini5 <gini5@163.com>
2018-10-12 17:32:02 +0800

    add game main code

---
+++ Pikachu/Main.py
@@ -0,0 +1,13 @@
+import os, cocos, struct
+from cocos.sprite import Sprite
+from classes.Pikachu import Pikachu
+from classes.Block import Block
+
+class JumpPikachu(cocos.layer.ColorLayer):
+    is_event_handler = True
+    def __init__(self):
+        super(JumpPikachu, self).__init__(255, 255, 255, 255, 800, 600)
+
+if __name__ == '__main__':
+    cocos.director.director.init(caption="Pikachu")
+    cocos.director.director.run(cocos.scene.Scene(JumpPikachu()))
