850becb45d14283c322057bb78059997e48109da
Gini5 <gini5@163.com>
2018-10-15 14:43:12 +0800

    draw floor and pikachu

--- Pikachu/Main.py
+++ Pikachu/Main.py
@@ -1,4 +1,4 @@
-import os, cocos,struct
+import os, cocos, struct
 from cocos.sprite import Sprite
 from classes.Pikachu import Pikachu
 from classes.Block import Block
@@ -6,7 +6,28 @@ from classes.Block import Block
 class JumpPikachu(cocos.layer.ColorLayer):
     is_event_handler = True
     def __init__(self):
-        super(JumpPikachu, self).__init__(255, 255, 255, 255, 800, 600)
+        #initialize canvas
+        super(JumpPikachu, self).__init__(255, 255, 255, 255, 800, 1000)
+
+        self.numSamples = 1000
+
+        #voice control bar
+        self.vbar = Sprite('block.png')
+        self.vbar.position = 20,450
+        self.vbar.scale_y = 0.1     #bar height
+        self.vbar.image_anchor = 0,0
+        self.add(self.vbar)
+        #Pikachu instance
+        self.pikachu = Pikachu()
+        # self.add(self.pikachu)
+        #cocosnode
+        self.floor = cocos.cocosnode.CocosNode()
+        self.add(self.floor)
+        position = 0, 100
+        for i in range(1, 3):
+            block = Block(position)
+            self.floor.add(block)
+            position = block.x + block.width, block.height
 
 if __name__ == '__main__':
     cocos.director.director.init(caption="Pikachu")
--- Pikachu/classes/Block.py
+++ Pikachu/classes/Block.py
@@ -5,8 +5,10 @@ class Block(cocos.sprite.Sprite):
         self.image_anchor = 0, 0
         x, y = position
         if x == 0:
+            # to right and up scale
             self.scale_x = 4.5      # flat land with length 4.5
             self.scale_y = 1        # flat land with height 1
         else:       # generate block at random
             self.scale_x = 0.5 + random.random()*1.5
             self.scale_y = min(max(y-50+random.random()*100, 50), 300)/100
+            self.position = x+50+random.random()*100, 0
