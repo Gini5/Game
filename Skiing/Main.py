a2c9c9068a563abe0b64a49d19763b408807cc68
Gini <gini5@163.com>
2018-10-27 20:23:19 +0800

    Start Interface

--- Skiing/Main.py
+++ Skiing/Main.py
@@ -23,3 +23,23 @@ def AddObstacles(obstacles0, obstacles1):
 	for obstacle in obstacles1:
 		obstacles.add(obstacle)
 	return obstacles
+
+def Show_Start_Interface(Demo, width, height):
+	Demo.fill((255, 255, 255))
+	tfont = pygame.font.Font('./font/simkai.ttf', width//4)
+	cfont = pygame.font.Font('./font/simkai.ttf', width//20)
+	title = tfont.render(u'滑雪游戏', True, (255, 0, 0))
+	content = cfont.render(u'按任意键开始游戏', True, (0, 0, 255))
+	trect = title.get_rect()
+	trect.midtop = (width/2, height/10)
+	crect = content.get_rect()
+	crect.midtop = (width/2, height/2.2)
+	Demo.blit(title, trect)
+	Demo.blit(content, crect)
+	pygame.display.update()
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				return
