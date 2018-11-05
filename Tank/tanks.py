1f883149d5197dbe64594950b445c9d9dab53cb5
Gini5 <gini5@163.com>
2018-11-05 15:36:42 +0800

    update tank property

--- Tank/tanks.py
+++ Tank/tanks.py
@@ -12,3 +12,16 @@ class myTank(pygame.sprite.Sprite):
                           './images/myTank/tank_T2_2.png']
         else:
             raise ValueError('myTank class -> player value error.')
+        # tank level(initial 0)
+        self.level = 0
+        # load tank
+        self.tank = pygame.image.load(self.tanks[self.level]).convert_alpha()
+        self.tank_0 = self.tank.subsurface((0, 0), (48, 48))
+        self.tank_1 = self.tank.subsurface((48, 0), (48, 48))
+        self.rect = self.tank_0.get_rect()
+        # protected mask
+        self.protected_mask = pygame.image.load('./images/others/protect.png').convert_alpha()
+        self.protected_mask1 = self.protected_mask.subsurface((0, 0), (48, 48))
+        self.protected_mask2 = self.protected_mask.subsurface((48, 0), (48, 48))
+        # tank direction
+        self.direction_x, self.direction_y = 0, -1
