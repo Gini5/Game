4c9599f9fbef695fea4a1afa245c4f8f2083da71
Gini5 <gini5@163.com>
2018-11-06 10:35:43 +0800

    turn bullet direction

--- Tank/bullet.py
+++ Tank/bullet.py
@@ -13,3 +13,16 @@ class Bullet(pygame.sprite.Sprite):
         self.speed = 6
         self.alive = False
         self.stronger = False
+
+    def turn(self, direction_x, direction_y):
+        self.direction_x, self.direction_y = direction_x, direction_y
+        if self.direction_x == 0 and self.direction_y == -1:
+            self.bullet = pygame.image.load(self.bullets[0])
+        elif self.direction_x == 0 and self.direction_y == 1:
+            self.bullet = pygame.image.load(self.bullets[1])
+        elif self.direction_x == -1 and self.direction_y == 0:
+            self.bullet = pygame.image.load(self.bullets[2])
+        elif self.direction_x == 1 and self.direction_y == 0:
+            self.bullet = pygame.image.load(self.bullets[3])
+        else:
+            raise ValueError('Bullet class -> direction value error.')
