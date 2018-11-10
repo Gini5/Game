a9b0bcbc711cdada4e1b82a4977641819a45959d
Gini <gini5@163.com>
2018-11-10 15:34:01 +0800

    add enemy tank

--- Tank/tanks.py
+++ Tank/tanks.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from bullet import Bullet
 
 class myTank(pygame.sprite.Sprite):
@@ -191,7 +192,7 @@ class myTank(pygame.sprite.Sprite):
             if pygame.sprite.spritecollide(self, tankGroup, False, None):
                 self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
                 is_move = False
-            
+
             if pygame.sprite.collide_rect(self, myhome):
                 self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
                 is_move = False
@@ -213,3 +214,115 @@ class myTank(pygame.sprite.Sprite):
             else:
                 raise ValueError('myTank class -> player value error.')
             self.speed = 3
+
+    class enemyTank(pygame.sprite.Sprite):
+        def __init__(self, x=None, kind=None, is_red=None):
+            pygame.sprite.Sprite.__init__(self)
+            self.born = True
+            self.times = 90
+            if kind is None:
+                self.kind = random.randint(0, 3)
+            else:
+                self.kind = kind
+            self.tanks1 = ['./images/enemyTank/enemy_1_0.png', './images/enemyTank/enemy_1_1.png',
+                           './images/enemyTank/enemy_1_2.png', './images/enemyTank/enemy_1_3.png']
+            self.tanks2 = ['./images/enemyTank/enemy_2_0.png', './images/enemyTank/enemy_2_1.png',
+                           './images/enemyTank/enemy_2_2.png', './images/enemyTank/enemy_2_3.png']
+            self.tanks3 = ['./images/enemyTank/enemy_3_0.png', './images/enemyTank/enemy_3_1.png',
+                           './images/enemyTank/enemy_3_2.png', './images/enemyTank/enemy_3_3.png']
+            self.tanks4 = ['./images/enemyTank/enemy_4_0.png', './images/enemyTank/enemy_4_1.png',
+                           './images/enemyTank/enemy_4_2.png', './images/enemyTank/enemy_4_3.png']
+            self.tanks = [self.tanks1, self.tanks2, self.tanks3, self.tanks4]
+
+            if is_red is None:
+                self.is_red = random.choice((True, False, False, False, False))
+            else:
+                self.is_red = is_red
+            if self.is_red:
+                self.color = 3
+            else:
+                self.color = random.randint(0, 2)
+            self.blood = self.color
+            self.tank = pygame.image.load(self.tanks[self.kind][self.color]).convert_alpha()
+            self.tank_0 = self.tank.subsurface((0, 48), (48, 48))
+            self.tank_1 = self.tank.subsurface((48, 48), (48, 48))
+            self.rect = self.tank_0.get_rect()
+            if x is None:
+                self.x = random.randint(0, 2)
+            else:
+                self.x = x
+            self.rect.left, self.rect.top = 3 + self.x * 12 * 24, 3
+            self.can_move = True
+            self.speed = max(3 - self.kind, 1)
+            self.direction_x, self.direction_y = 0, 1
+            self.being = True
+            self.bullet = Bullet()
+
+        def shoot(self):
+            self.bullet.being = True
+            self.bullet.turn(self.direction_x, self.direction_y)
+            if self.direction_x == 0 and self.direction_y == -1:
+                self.bullet.rect.left = self.rect.left + 20
+                self.bullet.rect.bottom = self.rect.top - 1
+            elif self.direction_x == 0 and self.direction_y == 1:
+                self.bullet.rect.left = self.rect.left + 20
+                self.bullet.rect.top = self.rect.bottom + 1
+            elif self.direction_x == -1 and self.direction_y == 0:
+                self.bullet.rect.right = self.rect.left - 1
+                self.bullet.rect.top = self.rect.top + 20
+            elif self.direction_x == 1 and self.direction_y == 0:
+                self.bullet.rect.left = self.rect.right + 1
+                self.bullet.rect.top = self.rect.top + 20
+            else:
+                raise ValueError('enemyTank class -> direction value error.')
+
+        def move(self, tankGroup, brickGroup, ironGroup, myhome):
+            self.rect = self.rect.move(self.speed * self.direction_x, self.speed * self.direction_y)
+            is_move = True
+            if self.direction_x == 0 and self.direction_y == -1:
+                self.tank_0 = self.tank.subsurface((0, 0), (48, 48))
+                self.tank_1 = self.tank.subsurface((48, 0), (48, 48))
+                if self.rect.top < 3:
+                    self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                    self.direction_x, self.direction_y = random.choice(([0, 1], [0, -1], [1, 0], [-1, 0]))
+                    is_move = False
+            elif self.direction_x == 0 and self.direction_y == 1:
+                self.tank_0 = self.tank.subsurface((0, 48), (48, 48))
+                self.tank_1 = self.tank.subsurface((48, 48), (48, 48))
+                if self.rect.bottom > 630 - 3:
+                    self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                    self.direction_x, self.direction_y = random.choice(([0, 1], [0, -1], [1, 0], [-1, 0]))
+                    is_move = False
+            elif self.direction_x == -1 and self.direction_y == 0:
+                self.tank_0 = self.tank.subsurface((0, 96), (48, 48))
+                self.tank_1 = self.tank.subsurface((48, 96), (48, 48))
+                if self.rect.left < 3:
+                    self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                    self.direction_x, self.direction_y = random.choice(([0, 1], [0, -1], [1, 0], [-1, 0]))
+                    is_move = False
+            elif self.direction_x == 1 and self.direction_y == 0:
+                self.tank_0 = self.tank.subsurface((0, 144), (48, 48))
+                self.tank_1 = self.tank.subsurface((48, 144), (48, 48))
+                if self.rect.right > 630 - 3:
+                    self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                    self.direction_x, self.direction_y = random.choice(([0, 1], [0, -1], [1, 0], [-1, 0]))
+                    is_move = False
+            else:
+                raise ValueError('enemyTank class -> direction value error.')
+            if pygame.sprite.spritecollide(self, brickGroup, False, None) \
+                    or pygame.sprite.spritecollide(self, ironGroup, False, None) \
+                    or pygame.sprite.spritecollide(self, tankGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                self.direction_x, self.direction_y = random.choice(([0, 1], [0, -1], [1, 0], [-1, 0]))
+                is_move = False
+            if pygame.sprite.collide_rect(self, myhome):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                self.direction_x, self.direction_y = random.choice(([0, 1], [0, -1], [1, 0], [-1, 0]))
+                is_move = False
+            return is_move
+
+
+        def reload(self):
+            self.tank = pygame.image.load(self.tanks[self.kind][self.color]).convert_alpha()
+            self.tank_0 = self.tank.subsurface((0, 48), (48, 48))
+            self.tank_1 = self.tank.subsurface((48, 48), (48, 48))
