431c54e5c9f4db7400fbb73e49aaea8561ff44f6
Gini <gini5@163.com>
2018-11-10 15:31:39 +0800

    tank move and reset

--- Tank/tanks.py
+++ Tank/tanks.py
@@ -115,3 +115,101 @@ class myTank(pygame.sprite.Sprite):
                 self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
                 is_move = False
             return is_move
+
+        def move_down(self, tankGroup, brickGroup, ironGroup, myhome):
+            self.direction_x, self.direction_y = 0, 1
+            self.rect = self.rect.move(self.speed * self.direction_x, self.speed * self.direction_y)
+            self.tank_0 = self.tank.subsurface((0, 48), (48, 48))
+            self.tank_1 = self.tank.subsurface((48, 48), (48, 48))
+            is_move = True
+
+            if self.rect.bottom > 630 - 3:
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.spritecollide(self, brickGroup, False, None) or \
+                    pygame.sprite.spritecollide(self, ironGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.spritecollide(self, tankGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.collide_rect(self, myhome):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+            return is_move
+
+
+        def move_left(self, tankGroup, brickGroup, ironGroup, myhome):
+            self.direction_x, self.direction_y = -1, 0
+
+            self.rect = self.rect.move(self.speed * self.direction_x, self.speed * self.direction_y)
+            self.tank_0 = self.tank.subsurface((0, 96), (48, 48))
+            self.tank_1 = self.tank.subsurface((48, 96), (48, 48))
+
+            is_move = True
+
+            if self.rect.left < 3:
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.spritecollide(self, brickGroup, False, None) or \
+                    pygame.sprite.spritecollide(self, ironGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.spritecollide(self, tankGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.collide_rect(self, myhome):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+            return is_move
+
+
+        def move_right(self, tankGroup, brickGroup, ironGroup, myhome):
+            self.direction_x, self.direction_y = 1, 0
+
+            self.rect = self.rect.move(self.speed * self.direction_x, self.speed * self.direction_y)
+            self.tank_0 = self.tank.subsurface((0, 144), (48, 48))
+            self.tank_1 = self.tank.subsurface((48, 144), (48, 48))
+
+            is_move = True
+
+            if self.rect.right > 630 - 3:
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.spritecollide(self, brickGroup, False, None) or \
+                    pygame.sprite.spritecollide(self, ironGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+
+            if pygame.sprite.spritecollide(self, tankGroup, False, None):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+            
+            if pygame.sprite.collide_rect(self, myhome):
+                self.rect = self.rect.move(self.speed * -self.direction_x, self.speed * -self.direction_y)
+                is_move = False
+            return is_move
+
+        #reset after dead
+        def reset(self):
+            self.level = 0
+            self.protected = False
+            self.tank = pygame.image.load(self.tanks[self.level]).convert_alpha()
+            self.tank_0 = self.tank.subsurface((0, 0), (48, 48))
+            self.tank_1 = self.tank.subsurface((48, 0), (48, 48))
+            self.rect = self.tank_0.get_rect()
+            self.direction_x, self.direction_y = 0, -1
+            if self.player == 1:
+                self.rect.left, self.rect.top = 3 + 24 * 8, 3 + 24 * 24
+            elif self.player == 2:
+                self.rect.left, self.rect.top = 3 + 24 * 16, 3 + 24 * 24
+            else:
+                raise ValueError('myTank class -> player value error.')
+            self.speed = 3
