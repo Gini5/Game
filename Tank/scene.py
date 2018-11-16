b43b5a6d358fa359eff84b1ecf2e115cdc6af0bb
Gini5 <gini5@163.com>
2018-11-16 14:15:00 +0800

    bug fix

--- Tank/scene.py
+++ Tank/scene.py
@@ -16,3 +16,21 @@ class Iron(pygame.sprite.Sprite):
 		self.iron = pygame.image.load('./images/scene/iron.png')
 		self.rect = self.iron.get_rect()
 		self.being = False
+
+class Ice(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.ice = pygame.image.load('./images/scene/ice.png')
+		self.rect = self.ice.get_rect()
+		self.being = False
+
+
+class River(pygame.sprite.Sprite):
+	def __init__(self, kind=None):
+		pygame.sprite.Sprite.__init__(self)
+		if kind is None:
+			self.kind = random.randint(0, 1)
+		self.rivers = ['./images/scene/river1.png', './images/scene/river2.png']
+		self.river = pygame.image.load(self.rivers[self.kind])
+		self.rect = self.river.get_rect()
+		self.being = False
