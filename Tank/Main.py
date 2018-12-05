00f33af9bd4ae790d147a838b0ee2932f10f54b0
Gini5 <gini5@163.com>
2018-12-05 15:07:33 +0800

    show swtich interface

--- Tank/Main.py
+++ Tank/Main.py
@@ -47,3 +47,21 @@ def show_end_interface(screen, width, height, is_win):
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				sys.exit()
+
+def show_switch_stage(screen, width, height, stage):
+	bg_img = pygame.image.load("./images/others/background.png")
+	screen.blit(bg_img, (0, 0))
+	font = pygame.font.Font('./font/simkai.ttf', width//10)
+	content = font.render(u'第%d关' % stage, True, (0, 255, 0))
+	rect = content.get_rect()
+	rect.midtop = (width/2, height/2)
+	screen.blit(content, rect)
+	pygame.display.update()
+	delay_event = pygame.constants.USEREVENT
+	pygame.time.set_timer(delay_event, 1000)
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				sys.exit()
+			if event.type == delay_event:
+				return
